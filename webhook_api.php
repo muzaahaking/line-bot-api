b9ae1d4706768a221c4a82ee0f6a2e7e3b8a1bb0
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2021-10-23 15:28:28 +0700

    Update webhook_api.php

--- webhook_api.php
+++ webhook_api.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = '5EThX6Vk3J1vG2loJYE3sBL+2RIPhStAsGNsWl60p+5OUJxMArXpnRTdY42INPSRBzLQ+YMi3ZYGQ9IYMQ7xUxzJ5sP91Tl9mxn8sndw6wucWSKupVtPQeJqU3rn/HrAqGqllTSVAc/2awUFwsrHIgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'M2a6HOz5+QpBbctXqOzlOSSsv9t+s3OT4SJUyy1QzkbOoH9NioT1rmEY6SYm6PeIiZ7GY2N12odRGxWAHhuSFoePGOE/qLJKhrq98Nwfb6zVLgC7Np9YEFr6tBkDIlVENCDg3NPbb4t0e1tBGaet2QdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
